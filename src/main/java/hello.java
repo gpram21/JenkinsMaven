693dcdc97fd6a2ec8eb824c84b5c53665500ede2
Prabhu <prabhuramchennai@gmail.com>
2020-05-18 18:12:28 +0200

    pushed from local

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("**********************Jenkins_Gi1t******99999999**********");
+        System.out.println("**********************Changed from local**********");
     }
 }
