5eb536d642dc00196360c5662bd93b9ef041252d
Prabhu <prabhuramchennai@gmail.com>
2020-05-18 16:54:21 +0200

    new

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("**********************Jenkins");
+        System.out.println("**********************Jenkins_Git");
     }
 }
