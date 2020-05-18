541d49773c611d1eb8076be4bc2a0d845f4d6a83
gpram21 <prabhuramchennai@gmail.com>
2020-05-18 17:59:45 +0200

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("**********************Jenkins_Git");
+        System.out.println("**********************Jenkins_Git****************");
     }
 }
