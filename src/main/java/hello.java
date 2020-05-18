48695c951411fae58d77e1b14c888f05d91cb605
gpram21 <prabhuramchennai@gmail.com>
2020-05-18 18:15:08 +0200

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("**********************pulled to  local**********");
+        System.out.println("**********************fetch and merge**********");
     }
 }
